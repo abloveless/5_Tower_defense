2a7a5f0ef4bead60091c263f8ca4452cf8ee3c0f
DESKTOP-STAV456\Doomy <abloveless12@gmail.com>
2018-09-15 18:47:42 -0600

    Got collsions working

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AddBoxCollider();
+	}
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>(); // generate Box Collider
+        boxCollider.isTrigger = false; // set box collider trigger to false
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        print("Bullets hit something");
+        //ProcessHit();
+        //if (hits <= 0)
+        //{
+        //    KillEnemy();
+        //}
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
