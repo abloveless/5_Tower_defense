96e78202e9f8ce50a92632d0ac3b1b88211c0e08
DESKTOP-STAV456\Doomy <abloveless12@gmail.com>
2018-09-15 19:00:48 -0600

    Particles collide, hit enemy, reduce health (serializeField), and destroy game object

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField] int hits = 10;
+
+    // Use this for initialization
+    void Start () {
         AddBoxCollider();
 	}
 
@@ -18,11 +21,23 @@ public class Enemy : MonoBehaviour {
     void OnParticleCollision(GameObject other)
     {
         print("Bullets hit something");
-        //ProcessHit();
-        //if (hits <= 0)
-        //{
-        //    KillEnemy();
-        //}
+        ProcessHit();
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        print("Enemy dead");
+        Destroy(gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        hits = hits - 1;
+        // todo consider hit FX
     }
 
     // Update is called once per frame
