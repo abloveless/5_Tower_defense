e0c831cdce5a6005d0402ce3a7faa910028a718d
DESKTOP-EMBGUG3\abloveless <abloveless12@gmail.com>
2018-09-21 15:13:37 -0500

    Enemy explodes on base

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
 
     [SerializeField] float movementPeriod = 0.5f;
+    [SerializeField] ParticleSystem explodeParticlePrefab;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         PathFinder pathfinder = FindObjectOfType<PathFinder>();
         var path = pathfinder.GetPath();
         StartCoroutine(FollowPath(path));
-	}
+    }
 
     IEnumerator FollowPath(List<Waypoint> path)
     {
@@ -22,8 +25,14 @@ public class EnemyMovement : MonoBehaviour {
             transform.position = waypoint.transform.position;
             yield return new WaitForSeconds(movementPeriod);
         }
-        print("Ending patrol...");
+        DestroyEnemy();
     }
 
-
+    private void DestroyEnemy()
+    {
+        var vfx = Instantiate(explodeParticlePrefab, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, vfx.main.duration);
+        Destroy(gameObject); // the enemy
+    }
 }
