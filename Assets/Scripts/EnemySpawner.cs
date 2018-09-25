f764ddfe311e7376c4458fe70b77e50ec53d3e1d
DESKTOP-STAV456\Doomy <abloveless12@gmail.com>
2018-09-24 22:49:21 -0400

    Wave counter in, but has bug

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,25 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
     [Range(0.1f, 10f)]
     [SerializeField] float secondsBetweenSpawn = .5f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Text spawnedEnemies;
+
+    int score;
+    
+
 	// Use this for initialization
 	void Start () {
+        spawnedEnemies.text = score.ToString();
         StartCoroutine(SpawnEnemies());
+        
 	}
 	
-    private IEnumerator SpawnEnemies()
+    IEnumerator SpawnEnemies()
     {
         while (true) // forever
         {
+            AddScore();
             var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            print("Spawned Enemy");
             newEnemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawn);
         }
     }
 
+    private void AddScore()
+    {
+        print("Adding 5");
+        score = score + 5;
+        spawnedEnemies.text = score.ToString();
+    }
 }
