8dac6730b3747c4538814fb900d3194b0e32d547
DESKTOP-STAV456\Doomy <abloveless12@gmail.com>
2018-09-24 21:06:48 -0400

    Lecture 157 finished

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] int playerHealth = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        playerHealth = playerHealth - 1;
+    }
+}
